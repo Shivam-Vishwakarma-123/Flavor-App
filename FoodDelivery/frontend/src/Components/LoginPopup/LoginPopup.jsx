a91f0b9fd7f6f5a73a2bba19af4e8fdd4b45f591
Shivam Vishwakarma <shivam@example.com>
2024-09-24 16:58:19 +0530

    initial

--- FoodDelivery/frontend/src/Components/LoginPopup/LoginPopup.jsx
+++ FoodDelivery/frontend/src/Components/LoginPopup/LoginPopup.jsx
@@ -5,7 +5,7 @@ import { StoreContext } from "../../Context/Context";
 import axios from "axios";
 const LoginPopup = ({ setShowLogin }) => {
   const { url, setToken } = useContext(StoreContext);
-  const [currentState, setCurrentState] = useState("Sign Up");
+  const [currentState, setCurrentState] = useState("Login");
   const [data, setData] = useState({
     name: "",
     email: "",
