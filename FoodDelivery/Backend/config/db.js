fcca382984b7b163e20c0a734aa9e2350e99a12c
Shivam Vishwakarma <shivam@example.com>
2025-01-12 12:27:52 +0530

    db connected

--- FoodDelivery/Backend/config/db.js
+++ FoodDelivery/Backend/config/db.js
@@ -4,5 +4,6 @@ import mongoose from "mongoose";
 //     await mongoose.connect('mongodb+srv://Shivam:MongoDB@cluster0.v43bs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(()=>console.log("DB Connected"));
 // }
 export const connectDB=async()=>{
-    await mongoose.connect('mongodb+srv://Shivam:MongoDB@cluster0.v43bs.mongodb.net/food').then(()=>console.log("DB Connected"));
+    // await mongoose.connect('mongodb+srv://Shivam:MongoDB@cluster0.v43bs.mongodb.net/food').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://Shivam:MongoDB%4018042002@cluster0.g8pab.mongodb.net/food').then(()=>console.log("DB Connected"));
 }
