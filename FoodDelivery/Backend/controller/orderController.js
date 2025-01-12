fb915aae82360bfb8dace20c693a9a051387ebec
Shivam Vishwakarma <shivam@example.com>
2025-01-12 20:18:53 +0530

    initial

--- FoodDelivery/Backend/controller/orderController.js
+++ FoodDelivery/Backend/controller/orderController.js
@@ -4,7 +4,7 @@ import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 // placing user order from frontend 
 const placeOrder = async (req, res) => {
- const frontend_URL="http://localhost:5173";
+ const frontend_URL="https://flavor-app-frontend.onrender.com/";
     try {
         const newOrder = new orderModel({
             userId: req.body.userId,
